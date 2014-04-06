d2cd5fa01ddb893887d85aee5cdd9b7710703ba1
divhenr <henrique_graca@hotmail.com>
2014-04-06 20:32:11 +0100

    v.9.0
    Added comments, JQM 1.4.2, bug fixes

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,14 @@
 var mobilepub = (function () {
 	return {
 		setTitle: function(title){
-			$("div[data-role=header]:visible h1").text(title);
+			$("div[data-role=header]:not(.head):visible h1").text(title);
+			mobilepub.diagram.diagramTitle = title;
 		},
 
 		load: function(){
 			$.getJSON("settings.json", function(data) {
 				mobilepub.settings = data;
-				mobilepub.getTitle();
+				mobilepub.getProperties();
 				$("#appVersion").text(data.version);
 				$(".datetime").text(" [" + data.dateTime + "] ");
 
@@ -15,13 +16,24 @@ var mobilepub = (function () {
 				mobilepub.buildCategoryStruct();
 			});
 		},
-		getTitle: function(){
+		getProperties: function(){
 			$.ajax({
 				type: "GET",
 				url: mobilepub.settings.publicationpath + "publicationproperties.xml",
 				dataType: "xml",
 				success: function(xml) {				
 					mobilepub.title = $(xml).find("PublicationProperties > Title").text();
+					var canSend =$(xml).find("PublicationFeedback").attr("Enabled");
+					if(canSend != undefined && canSend.toLowerCase() != "false"){
+						canSend = true;
+					}else{
+						canSend = false;
+					}
+					mobilepub.feedback ={
+						canSendFeedback:  canSend,
+						email:  $(xml).find("PublicationContacts > Contact").attr("Email"),
+					};
+
 					$("#apptitle").text(mobilepub.title);
 					document.title = mobilepub.title;
 				}
@@ -333,13 +345,15 @@ currentShape = currentShape;
         								sWidth = dimensions.width.value * scaleX,
         								sHeight = dimensions.height.value * scaleY;
 
+									            xLong += sWidth;
+									            yLong += sHeight;
 										// These give values for xLong / yLong that give the CENTRE of the row/column
 										// of meta-data indicators.  Individual MDI locations are set in the loop below
 									        // if (this.showMDIsPosition == 'left') {
 									        //     yLong += Math.floor(sHeight / 2);
 									        // } else if (this.showMDIsPosition == 'right') {
-									            xLong += sWidth;
-									            yLong += Math.floor(sHeight / 2);
+									            // xLong += sWidth;
+									            // yLong += Math.floor(sHeight / 2);
 									        // } else if (this.showMDIsPosition == 'top') {
 									        //     xLong += Math.floor(sWidth / 2);
 									        // } else if (this.showMDIsPosition == 'bottom') {
@@ -356,7 +370,12 @@ currentShape = currentShape;
 									if(isDiagLink){
 
 										$(div).find("img").attr("src", mobilepub.settings.imagesFolder + "hasdiaglinks.gif");
-										$(div).css({"top": yLong, "left": xLong - $(".responsive-image").width()});
+
+										if(!isDocLink && !isIssues){
+											$(div).css({"top": yLong - $(".responsive-image").width(), "left": xLong - $(".responsive-image").width()});	
+										}else{
+											$(div).css({"top": yLong, "left": xLong - $(".responsive-image").width()});
+										}
 
 										$(div).clone().prependTo("#imagescroller");
 										//$("#imagescroller").prepend(diagDiv);
@@ -365,7 +384,7 @@ currentShape = currentShape;
 									if(isIssues){
 										$(div).find("img").attr("src", mobilepub.settings.imagesFolder + "hasissues.gif");
 										$(div).css({"top": yLong - $(".responsive-image").width(), "left": xLong - $(".responsive-image").width()});
-										$(div).attr("onclick",'mobilepub.showShapeInfoByFile(\''+ source +'\')');
+										$(div).attr("onclick",'mobilepub.showShapeInfoByFile(\''+ source +'\',\'Issues\')');
 										
 										$(div).clone().prependTo("#imagescroller");
 									}
@@ -652,7 +671,7 @@ currentShape = currentShape;
 				$.mobile.changePage('/partials/diagram.html?id='+ docId + "&page=" + pageId + "&shapeid=" + shapeId);
 			}
 		},
-		showShapeInfo: function(path, target){
+		showShapeInfo: function(path, target, openInfoLevel){
 			$.ajax({
 				type: "GET",
 				url: path,
@@ -660,6 +679,7 @@ currentShape = currentShape;
 				success: function(xml) {
 					mobilepub.diagram.current = mobilepub.buildDiagramInfo(xml);
 
+					$( "[data-role=collapsible-set]" ).children().collapsible( "collapse" );
 					if(target){
 						$(target).trigger('alwaysOn.maphilight');
 					}
@@ -669,14 +689,21 @@ currentShape = currentShape;
 					}else{
 						$("#diagraminfopanel").panel("open");
 					}
+
+					if(openInfoLevel != undefined){
+						if(openInfoLevel == "Issues"){
+							$("[data-id=collissues]").collapsible( "expand" );
+						}
+					}
+
 				},error:function(){
-					$("#diagraminfopanel").panel("close");
+					// $("#diagraminfopanel").panel("close");
 				}
 			});
 		},
-		showShapeInfoByFile: function(file){
+		showShapeInfoByFile: function(file, openInfoLevel){
 			var path = mobilepub.diagram.currentPath + file;
-			mobilepub.showShapeInfo(path);
+			mobilepub.showShapeInfo(path, undefined, openInfoLevel);
 		},
 		navigateToChild: function(pageId, shapeId, source, action){
 			$.ajax({
@@ -752,17 +779,10 @@ currentShape = currentShape;
 
 var downloadURL = function downloadURL(url) {
 
-	$("#filedownload").attr("href", "/partials/fileviewer.html?url=" + url).click();
-	//window.location = url;
-    // var hiddenIFrameID = 'hiddenDownloader',
-    //     iframe = document.getElementById(hiddenIFrameID);
-    // if (iframe === null) {
-    //     iframe = document.createElement('iframe');
-    //     iframe.id = hiddenIFrameID;
-    //     iframe.style.display = 'none';
-    //     document.body.appendChild(iframe);
-    // }
-    // iframe.src = url;
+	//// if want to show the document in a jqm window
+	//$("#filedownload").attr("href", "/partials/fileviewer.html?url=" + url).click();
+
+	window.location = url;
 };
 
 function OnShapeClick(a,b,evt){
@@ -774,9 +794,9 @@ function OnShapeClick(a,b,evt){
 	mobilepub.clearAllAreaStyles();
 
 	if(!isOn){
-		if($("#diagraminfopanel").data("mobilePanel")._open){
-			$("#diagraminfopanel").panel( "close" );
-		}
+		// if($("#diagraminfopanel").data("mobilePanel")._open){
+		// 	$("#diagraminfopanel").panel( "close" );
+		// }
 	}else{
 		// show info
 		var path = mobilepub.diagram.currentPath + a + "_" + b + '.xml';
@@ -822,6 +842,7 @@ $(document).on("mobileinit", function(event){
 	//$.mobile.orientationChangeEnabled = false;
 });
 
+
 // $(document).on("pagebeforeshow", '#browsediagrampage',function(event, data){
 // 	// if(!mobilepub.diagram.init){
 // 	// 	mobilepub.buildDiagramStruct();
@@ -836,6 +857,11 @@ $(document).on("pageshow", '#browsediagrampage',function(event, data){
 $(document).on("pageshow", '#diagrampage',function(event, data){
 	var parameters = $(this).data("url").split("?")[1];
 	mobilepub.loadDiagramImage(getQueryVariable(parameters, "id"), getQueryVariable(parameters, "page"), getQueryVariable(parameters, "shapeid"));
+
+if(mobilepub.feedback.canSendFeedback){
+	$("#commentbtn").removeClass('ui-disabled');
+	$("form#commentform input[name=email]").val(mobilepub.feedback.email);
+}
 
 // $('img[usemap]').load(function(){
 // 	console.log("s");
@@ -954,6 +980,32 @@ $( document ).on("pageinit", "#searchpage", function() {
     });
 });
 
+$(document).on("click", '#sendComment',function(event, data){
+	
+	var form = $("form#commentform").serializeObject(),
+		subject = "New comment to " + mobilepub.diagram.diagramTitle,
+		body = form.name + " commented on " + mobilepub.diagram.diagramTitle + "%0D%0A%0D%0AComment:%0D%0A" + form.comment
+
+	if(form.name == "" || form.comment == ""){
+		return;
+	}
+	// if(!window.mobilecheck()){
+	// 	window.open('mailto:'+ form.email +'?subject='+subject+'&body=' + body);
+	// 	$("#commentpopup").popup( "close" );
+
+	// }else{
+		window.location = 'mailto:'+ form.email +'?subject='+subject+'&body=' + body;	
+	// }
+
+	$("#commentpopup").popup("close");
+	$("form#commentform").find("input, textarea").val("");
+	$("form#commentform input[name=email]").val(mobilepub.feedback.email);
+});
+
+$(document).on("click", '#cancelComment',function(event, data){
+	$("form#commentform").find("input, textarea").val("");
+	$("form#commentform input[name=email]").val(mobilepub.feedback.email);
+});
 
 $.expr[':'].iAttrStart = function(obj, params, meta, stack) {
     var opts = meta[3].match(/(.*)\s*,\s*(.*)/);
@@ -965,4 +1017,20 @@ var check = false;
 (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
 return check; }
 
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
 
